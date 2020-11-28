3a5ae1260b8d47b0c9b070ad7bd665f828a3d7a6
ericj79 <ericj79@gmail.com>
2020-11-28 10:55:05 -0500

    new fav icon

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'xxx',
+    authDomain: 'west-chester-rs.firebaseapp.com',
+    databaseURL: 'https://west-chester-rs.firebaseio.com/',
+    projectId: 'west-chester-rs',
+    storageBucket: 'west-chester-rs.appspot.com',
+    messagingSenderId: 'xxx',
+    appId: '1:xxx:web:xxx',
+  },
 };
